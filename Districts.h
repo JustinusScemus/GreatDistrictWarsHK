d76ec6f2ef33f315c64ec8d94cda49a876f7ee0e
Justin Sham <justinchifu@hotmail.com>
2021-09-18 18:51:20 +0800

    Improved "Districts.h"

--- Districts.h
+++ Districts.h
@@ -1,13 +1,15 @@
 //Two types of objects: Districts and Colours
 #include <string>
-class Colours {
+class Colour {
   private:
     std::string name;
     int argb; //argb means the colour shown on screen
 };
 
 class District {
+  //Districts are physical districts that are assigned a colour but changes during the game
   private:
     int landpower; //Islands is assigned to be largest while Y.T.M. is assigned to be smallest
     int fiscalpower; //C&W assigned to be largest while Islands assigned to be smallest
+    Colour& currcolor;
 };
